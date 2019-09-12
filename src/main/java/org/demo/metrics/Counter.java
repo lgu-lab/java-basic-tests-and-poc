964c9591bddd3bc56c289318248d6202353c4511
l-gu <laurent.guer1@gmail.com>
2019-09-12 18:48:25 +0200

    Metrics counters added

---
+++ src/main/java/org/demo/metrics/Counter.java
@@ -0,0 +1,51 @@
+package org.demo.metrics;
+
+public class Counter {
+
+	private final String name ;
+	private final long   threadId;
+	private long count = 0 ;
+	
+	public Counter(String name) {
+		super();
+		this.name = name;
+		this.count = 0L;
+		this.threadId = 0L;
+	}
+	
+	public Counter(String name, long count) {
+		super();
+		this.name = name;
+		this.count = count;
+		this.threadId = 0L;
+	}
+
+	public Counter(String name, long count, long threadId) {
+		super();
+		this.name = name;
+		this.count = count;
+		this.threadId = threadId;
+	}
+
+	public void increment() {
+		count++;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public long getThreadId() {
+		return threadId;
+	}
+	
+	public long getCount() {
+		return count;
+	}
+
+	@Override
+	public String toString() {
+		return "Counter '" + name + "' (" + threadId + ") = " + count;
+	}
+	
+}
---
+++ src/main/java/org/demo/metrics/UsageByThread.java
@@ -0,0 +1,27 @@
+package org.demo.metrics;
+
+public class UsageByThread {
+
+	public static void main(String[] args) {
+		
+		Counter c1 = CountersForCurrentThread.getCounter("c1") ;
+		Counter c2 = CountersForCurrentThread.getCounter("c2") ;
+		c1.increment();
+		c1.increment();
+		c2.increment();
+		c2.increment();
+		System.out.println("c1 : " + c1);
+		System.out.println("c2 : " + c2);
+		
+		Counter c1bis = Counters.getCounter("c1") ;
+		c1bis.increment();
+		System.out.println("c1 : " + c1);
+		System.out.println("c1bis : " + c1bis);
+		c1.increment();
+		c1.increment();
+		System.out.println("c1 : " + c1);
+		System.out.println("c1bis : " + c1bis);
+	}
+	
+	
+}
